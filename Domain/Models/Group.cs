c70bac7f52bab0c0ab96a1eec2a0ca6bfc2b328c
Giovanni Lucca Reque Uchoa <giovanni.uchoa@outlook.com>
2025-03-20 20:49:49 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Models/Group.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DocumentinAPI.Domain.Models
+{
+    public class Group
+    {
+
+        [Key]
+        public int GroupId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ Domain/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DocumentinAPI.Domain.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public short Profile { get; set; }
+        public short PreferredLanguage { get; set; }
+        public short PreferredTheme { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime LastLoginAt { get; set; }
+
+    }
+}
