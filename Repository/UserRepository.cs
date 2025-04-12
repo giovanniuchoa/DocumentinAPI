487354fca93f0a2202d767e70abd40904c1b53e0
Giovanni Lucca Reque Uchoa <giovanni.uchoa@outlook.com>
2025-04-12 18:24:46 -0300

    Task 2905: Implementar GETs de usuários com a devida validação de acesso

---
+++ Repository/UserRepository.cs
@@ -0,0 +1,75 @@
+﻿using DocumentinAPI.Data;
+using DocumentinAPI.Domain.DTOs.User;
+using DocumentinAPI.Domain.Utils;
+using DocumentinAPI.Interfaces.IRepository;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace DocumentinAPI.Repository
+{
+    public class UserRepository : BaseRepository, IUserRepository
+    {
+
+        public UserRepository(DBContext context) : base(context)
+        {
+        }
+
+        public async Task<Retorno<UserResponseDTO>> GetUserByIdAsync(int userId, UserSession ssn)
+        {
+           
+            Retorno<UserResponseDTO> oRetorno = new Retorno<UserResponseDTO>();
+
+            try
+            {
+
+                var userDB = await _context.Users
+                    .Where(u => u.UserId == userId
+                        && u.IsActive == true
+                        && u.CompanyId.ToString() == ssn.CompanyId)
+                    .FirstOrDefaultAsync();
+
+                if (userDB == null)
+                {
+                    throw new Exception("notFound");
+                }
+
+                oRetorno.Objeto = userDB.Adapt<UserResponseDTO>();
+                oRetorno.SetSucesso();
+
+            }
+            catch (Exception ex)
+            {
+                oRetorno.SetErro(ex.Message);  
+            }
+
+            return oRetorno;
+
+        }
+
+        public async Task<Retorno<IEnumerable<UserResponseDTO>>> GetListUserAsync(UserSession ssn)
+        {
+            Retorno<IEnumerable<UserResponseDTO>> oRetorno = new();
+
+            try
+            {
+
+                var userListDB = await _context.Users
+                    .Where(u => u.IsActive == true
+                        && u.CompanyId.ToString() == ssn.CompanyId)
+                    .ToListAsync();
+
+                oRetorno.Objeto = userListDB.Adapt<List<UserResponseDTO>>();
+
+                oRetorno.SetSucesso();
+
+            }
+            catch (Exception ex)
+            {
+                oRetorno.SetErro(ex.Message);
+            }
+
+            return oRetorno;
+
+        }
+    }
+}
