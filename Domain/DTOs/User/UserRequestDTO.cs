72f95d711ed03725c0b8923665494bcb8512c1be
Giovanni Lucca Reque Uchoa <giovanni.uchoa@outlook.com>
2025-04-14 15:17:19 -0300

    Product Backlog Item 896: API - CRUD Usuários

--- Domain/DTOs/User/UserRequestDTO.cs
+++ Domain/DTOs/User/UserRequestDTO.cs
@@ -1,6 +1,24 @@
-﻿namespace DocumentinAPI.Domain.DTOs.User
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DocumentinAPI.Domain.DTOs.User
 {
     public class UserRequestDTO
     {
+
+        public int? UserId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public short Profile { get; set; }
+
+        public short PreferredLanguage { get; set; }
+
+        public short PreferredTheme { get; set; }
+
     }
 }
