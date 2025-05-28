a5335966aad7ab44e4abd666a9da8071c064c9aa
Giovanni Uchoa <giovanni.uchoa@camposdealer.com>
2025-05-28 15:32:27 -0300

    Task 4761: Documentar os endpoints das controllers

--- Controllers/CompanyController.cs
+++ Controllers/CompanyController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DocumentinAPI.Controllers
 {
+
     [Route("Company")]
     [Authorize]
     public class CompanyController : BaseController
@@ -18,6 +19,13 @@ namespace DocumentinAPI.Controllers
             _service = service;
         }
 
+        /// <summary>
+        /// Obtém uma empresa pelo identificador.
+        /// </summary>
+        /// <response code="200">Retorna a empresa correspondente ao identificador.</response>
+        /// <response code="401">Usuário não autorizado.</response>
+        /// <response code="400">Se ocorrer algum erro inesperado.</response>
+        /// <response code="500">Erro interno do servidor.</response>
         [HttpGet("GetCompaniesById/{companyId}")]
         public async Task<IActionResult> GetCompaniesByIdAsync(int companyId)
         {
@@ -35,6 +43,13 @@ namespace DocumentinAPI.Controllers
 
         }
 
+        /// <summary>
+        /// Obtém uma lista de empresas.
+        /// </summary>
+        /// <response code="200">Retorna uma lista de empresas.</response>
+        /// <response code="401">Usuário não autorizado.</response>
+        /// <response code="400">Se ocorrer algum erro inesperado.</response>
+        /// <response code="500">Erro interno do servidor.</response>
         [HttpGet("GetListCompanies")]
         public async Task<IActionResult> GetListCompaniesAsync()
         {
@@ -70,6 +85,13 @@ namespace DocumentinAPI.Controllers
 
         }
 
+        /// <summary>
+        /// Cadastra uma empresa.
+        /// </summary>
+        /// <response code="200">Retorna o DTO de resposta da empresa cadastrada.</response>
+        /// <response code="401">Usuário não autorizado.</response>
+        /// <response code="400">Se ocorrer algum erro inesperado.</response>
+        /// <response code="500">Erro interno do servidor.</response>
         [HttpPost("AddCompany")]
         public async Task<IActionResult> AddCompanyAsync([FromBody] CompanyRequestDTO company)
         {
@@ -103,8 +125,15 @@ namespace DocumentinAPI.Controllers
                 return Forbid("noPermission");
             }
 
-        }        
-        
+        }
+
+        /// <summary>
+        /// Atualiza uma empresa.
+        /// </summary>
+        /// <response code="200">Retorna o DTO de resposta da empresa atualizada.</response>
+        /// <response code="401">Usuário não autorizado.</response>
+        /// <response code="400">Se ocorrer algum erro inesperado.</response>
+        /// <response code="500">Erro interno do servidor.</response>
         [HttpPut("UpdateCompany")]
         public async Task<IActionResult> UpdateCompanyAsync([FromBody] CompanyRequestDTO company)
         {
@@ -140,6 +169,13 @@ namespace DocumentinAPI.Controllers
 
         }
 
+        /// <summary>
+        /// Alterna o status (ativo/inativo) da empresa.
+        /// </summary>
+        /// <response code="200">Retorna o DTO de resposta da empresa atualizada.</response>
+        /// <response code="401">Usuário não autorizado.</response>
+        /// <response code="400">Se ocorrer algum erro inesperado.</response>
+        /// <response code="500">Erro interno do servidor.</response>
         [HttpPut("ToggleStatusCompany/{companyId}")]
         public async Task<IActionResult> ToggleStatusCompanyAsync(int companyId)
         {
--- Controllers/GroupController.cs
+++ Controllers/GroupController.cs
@@ -19,6 +19,13 @@ namespace DocumentinAPI.Controllers
             _service = service;
         }
 
+        /// <summary>
+        /// Obtém um Grupo pelo identificador.
+        /// </summary>
+        /// <response code="200">Retorna o grupo correspondente ao identificador.</response>
+        /// <response code="401">Usuário não autorizado.</response>
+        /// <response code="400">Se ocorrer algum erro inesperado.</response>
+        /// <response code="500">Erro interno do servidor.</response>
         [HttpGet("GetGroupById/{groupId}")]
         public async Task<IActionResult> GetGroupByIdAsync(int groupId)
         {
@@ -34,6 +41,13 @@ namespace DocumentinAPI.Controllers
             }
         }
 
+        /// <summary>
+        /// Obtém uma lista de Grupos.
+        /// </summary>
+        /// <response code="200">Retorna uma lista de grupos.</response>
+        /// <response code="401">Usuário não autorizado.</response>
+        /// <response code="400">Se ocorrer algum erro inesperado.</response>
+        /// <response code="500">Erro interno do servidor.</response>
         [HttpGet("GetListGroup")]
         public async Task<IActionResult> GetListGroupAsync()
         {
@@ -49,6 +63,13 @@ namespace DocumentinAPI.Controllers
             }
         }
 
+        /// <summary>
+        /// Cadastra um Grupo.
+        /// </summary>
+        /// <response code="200">Retorna o DTO de resposta do grupo cadastrado.</response>
+        /// <response code="401">Usuário não autorizado.</response>
+        /// <response code="400">Se ocorrer algum erro inesperado.</response>
+        /// <response code="500">Erro interno do servidor.</response>
         [HttpPost("AddGroup")]
         public async Task<IActionResult> AddGroupAsync([FromBody] GroupRequestDTO group)
         {
@@ -83,6 +104,13 @@ namespace DocumentinAPI.Controllers
 
         }
 
+        /// <summary>
+        /// Atualiza um Grupo.
+        /// </summary>
+        /// <response code="200">Retorna o DTO de resposta do grupo atualizado.</response>
+        /// <response code="401">Usuário não autorizado.</response>
+        /// <response code="400">Se ocorrer algum erro inesperado.</response>
+        /// <response code="500">Erro interno do servidor.</response>
         [HttpPut("UpdateGroup")]
         public async Task<IActionResult> UpdateGroupAsync([FromBody] GroupRequestDTO group)
         {
@@ -117,6 +145,13 @@ namespace DocumentinAPI.Controllers
 
         }
 
+        /// <summary>
+        /// Alterna o status (ativo/inativo) de um grupo.
+        /// </summary>
+        /// <response code="200">Retorna o DTO de resposta do grupo atualizado.</response>
+        /// <response code="401">Usuário não autorizado.</response>
+        /// <response code="400">Se ocorrer algum erro inesperado.</response>
+        /// <response code="500">Erro interno do servidor.</response>
         [HttpPut("ToggleStatusGroup/{groupId}")]
         public async Task<IActionResult> ToggleStatusGroupAsync(int groupId)
         {
@@ -151,6 +186,13 @@ namespace DocumentinAPI.Controllers
 
         }
 
+        /// <summary>
+        /// Obtém uma lista de usuários vinculados ao grupo correspondente ao identificador.
+        /// </summary>
+        /// <response code="200">Retorna uma lista de usuários vinculados ao grupo correspondente ao identificador.</response>
+        /// <response code="401">Usuário não autorizado.</response>
+        /// <response code="400">Se ocorrer algum erro inesperado.</response>
+        /// <response code="500">Erro interno do servidor.</response>
         [HttpGet("GetListUserByGroup/{groupId}")]
         public async Task<IActionResult> GetListUserXGroupByGroupAsync(int groupId)
         {
