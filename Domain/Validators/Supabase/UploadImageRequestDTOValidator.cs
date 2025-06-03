6b42fc0e0aead5d6a3b15f7d146d35264c293928
Giovanni Uchoa <giovanni.uchoa@camposdealer.com>
2025-06-03 12:04:54 -0300

    criando validação do dto de upload de imagem

---
+++ Domain/Validators/Supabase/UploadImageRequestDTOValidator.cs
@@ -0,0 +1,17 @@
+﻿using DocumentinAPI.Domain.DTOs.Supabase;
+using FluentValidation;
+
+namespace DocumentinAPI.Domain.Validators.Supabase
+{
+    public class UploadImageRequestDTOValidator : AbstractValidator<UploadImageRequestDTO>
+    {
+
+        public UploadImageRequestDTOValidator()
+        {
+            RuleFor(x => x.Image)
+                .NotNull().WithMessage("imageRequired");
+
+        }
+
+    }
+}
