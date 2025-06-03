d655c068ff7eca1319004c86f6c8c936dac8ee41
Giovanni Lucca Reque Uchoa <giovanni.uchoa@outlook.com>
2025-06-03 11:37:39 -0300

    documentação swagger do dto de upload de imagem

--- Domain/DTOs/Supabase/UploadImageRequestDTO.cs
+++ Domain/DTOs/Supabase/UploadImageRequestDTO.cs
@@ -1,7 +1,14 @@
 ﻿namespace DocumentinAPI.Domain.DTOs.Supabase
 {
+
+    /// <summary>
+    /// DTO de Requisição para fazer Upload de imagem na nuvem.
+    /// </summary>
     public class UploadImageRequestDTO
     {
+        /// <summary>
+        /// Imagem que será salva.
+        /// </summary>        
         public IFormFile Image { get; set; }
 
     }
