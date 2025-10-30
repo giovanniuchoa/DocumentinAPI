60b23cdf6881c99aa8c86a150413cd224e3c0c2d
Giovanni Uchoa <giovanni.uchoa@outlook.com>
2025-10-30 13:31:05 -0300

    chore: adicionar um validador para o dto de request da dashboard de documentos

---
+++ Domain/Validators/Document/DocumentDashboardRequestDTOValidator.cs
@@ -0,0 +1,19 @@
+﻿using DocumentinAPI.Domain.DTOs.Document;
+using FluentValidation;
+
+namespace DocumentinAPI.Domain.Validators.Document
+{
+    public class DocumentDashboardRequestDTOValidator : AbstractValidator<DocumentDashboardRequestDTO>
+    {
+        public DocumentDashboardRequestDTOValidator()
+        {
+            RuleFor(x => x.CreatedAtFrom)
+                .Must(x => x == null || x.Value != default(DateTime))
+                .WithMessage("invalidCreatedAtFromDate");
+
+            RuleFor(x => x.CreatedAtTo)
+                .Must(x => x == null || x.Value != default(DateTime))
+                .WithMessage("invalidCreatedAtToDate");
+        }
+    }
+}
